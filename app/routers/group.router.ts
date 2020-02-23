018a4ca55cd75b8c67b26f3870cd063ffe74d845
Jonatan Alvarez <jonatan_alvarez@epam.com>
2020-02-23 04:17:59 -0600

    Hometask 5 - Logging & error handling.

--- app/routers/group.router.ts
+++ app/routers/group.router.ts
@@ -1,6 +1,7 @@
 // group.router.js - Group route module
 import { GroupService } from '../services';
 import { validateSchema, groupSchema } from '.';
+import { logError } from '../logger';
 import express = require('express');
 
 const router = express.Router();
@@ -22,12 +23,14 @@ router.get('/:id', async (req, res) => {
     const group = await groupServiceInstance.getGroupById(id);
 
     if (!id) {
+        logError(req.method, req.params, 'NO ID PROVIDED');
         res.status(404).json({
             message: `NO ID PROVIDED`,
         }).end();
     }
 
     if (!group) {
+        logError(req.method, req.params, `GROUP WITH ID: '${id}'  NOT FOUND :(`);
         res.status(404).json({
             message: `GROUP WITH ID: '${id}'  NOT FOUND :(`,
         });
@@ -45,11 +48,13 @@ router.post('/', (req, res, next) => {
 router.post('/', validateSchema(groupSchema), async (req, res) => {
     const keysLength = Object.keys(req.body).length;
     if (keysLength === 0) {
+        logError(req.method, JSON.stringify(req.body), `Not payload found!`);
         res.status(400).json({
             message: `Not payload found!`,
         }).end();
     }
     if (keysLength < 3) {
+        logError(req.method, JSON.stringify(req.body), `Missing fields!`);
         res.status(400).json({
             message: `Missing fields!`,
         }).end();
@@ -61,12 +66,14 @@ router.post('/', validateSchema(groupSchema), async (req, res) => {
 
     const isIdAlreadyUsed = !!group;
     if (isIdAlreadyUsed) {
+        logError(req.method, JSON.stringify(req.body), `ID: '${id}' is already used`);
         res.status(404).json({
             message: `ID: '${id}' is already used`,
         });
     } else {
         const newGroup = await groupServiceInstance.createGroup({ id, name, permissions });
         if (!newGroup) {
+            logError(req.method, JSON.stringify(req.body), `Error creating new group with info ${newGroup}`);
             res.status(404).json({
                 message: `Error creating new group with info ${newGroup}`,
             });
@@ -89,12 +96,14 @@ router.put('/', validateSchema(groupSchema), async (req, res) => {
     const group = await groupServiceInstance.getGroupById(id);
 
     if (!group) {
+        logError(req.method, JSON.stringify(req.body), `GROUP WITH ID: '${id}'  NOT FOUND :(`);
         res.status(404).json({
             message: `GROUP WITH ID: '${id}'  NOT FOUND :(`,
         });
     } else {
         const updatedGroup = await groupServiceInstance.updateGroup(groupInfo);
         if (!updatedGroup) {
+            logError(req.method, JSON.stringify(req.body), `Error creating new group with info ${updatedGroup}`);
             res.status(404).json({
                 message: `Error updating info ${groupInfo} for group with ID: '${id}'`,
             });
@@ -130,6 +139,7 @@ router.delete('/', async (req, res) => {
     const groupServiceInstance = new GroupService();
     const deletedGroup = await groupServiceInstance.deleteGroup(id);
     if (!deletedGroup) {
+        logError(req.method, JSON.stringify(req.query), `Error deleting group with ID: '${id}'`);
         res.status(404).json({
             message: `Error deleting group with ID: '${id}'`,
         });
